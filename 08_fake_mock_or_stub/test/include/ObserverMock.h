f244bf8357de4e93ec67958fbb305ea31bb6b808
student <andrzejd@mailinator.com>
2019-12-16 15:37:08 +0100

    10_state_machine - first

--- 08_fake_mock_or_stub/test/include/ObserverMock.h
+++ 08_fake_mock_or_stub/test/include/ObserverMock.h
@@ -8,6 +8,6 @@
 
 class ObserverMock : public Observer{
 public:
-    MOCK_METHOD2(remember, void(std::string question, std::string answear));
+    MOCK_METHOD2(remember, void(std::string question, std::string answer));
     MOCK_CONST_METHOD1(answer, std::string(std::string question));
 };
